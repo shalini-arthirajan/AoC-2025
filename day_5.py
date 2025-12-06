c5bfe14925ce66913f3d66d68187f8c54a0ceaf9
Shalini Arthirajan <104921667+shalini-arthirajan@users.noreply.github.com>
2025-12-06 10:27:41 +0530

    Update day_5.py

--- day_5.py
+++ day_5.py
@@ -1,3 +1,4 @@
+# part 1
 count = 0
 ranges = []
 
@@ -20,5 +21,40 @@ with open('5_in.txt','r') as f:
                     break
 
 print(count)
+
+# part 2 https://youtu.be/hG9QDwiE28w?si=LWYog_JMsv3bnI1B merged intervals reference
+
+ranges = []
+
+with open('5_in.txt','r') as f:
+    for line in f:
+        line = line.strip()
+        if '-' in line:
+            a,b = map(int,line.split('-'))
+            ranges.append((a,b))
+
+ranges.sort()
+
+merged = []
+current_start, current_end = ranges[0]
+
+for a,b in ranges[1:]:
+    if a <= current_end+1: # checking to see if they touch or overlap
+        current_end = max(current_end,b)
+    else:
+        merged.append((current_start,current_end))
+        current_start,current_end = a,b
+
+merged.append((current_start,current_end))
+
+total = 0
+
+for start, end in merged:
+    range_len = end - start + 1  # number of values in this interval
+    total += range_len
+
+print(total)
+
+
 
 
